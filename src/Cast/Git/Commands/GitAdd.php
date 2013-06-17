b97288dee08f1f5f63a0637ab52b09f3ed345614
Jason Coward <jason@opengeek.com>
2013-06-17 08:53:06 -0600

    Update add options

--- src/Cast/Git/Commands/GitAdd.php
+++ src/Cast/Git/Commands/GitAdd.php
@@ -30,23 +30,32 @@ class GitAdd extends GitCommand
         $paths = implode(" ", $pathSpec);
 
         $command = $this->command;
-        if ($this->arg('dry-run', $args)) {
+        if ($this->arg('interactive', $args) || $this->arg('i', $args) || $this->arg('patch', $args) || $this->arg('p', $args)) {
+            throw new \RuntimeException("git interactive patch selection not supported by Cast");
+        }
+        if ($this->arg('edit', $args) || $this->arg('e', $args)) {
+            throw new \RuntimeException("git interactive patch editing not supported by Cast");
+        }
+        if ($this->arg('dry-run', $args) || $this->arg('n', $args)) {
             $command .= ' --dry-run';
             if ($this->arg('ignore-missing', $args)) $command .= ' --ignore-missing';
         }
         if ($this->arg('verbose', $args)) $command .= ' --verbose';
+        elseif ($this->arg('v', $args)) $command .= ' -v';
         if ($this->arg('force', $args)) $command .= ' --force';
-        if ($this->arg('update', $args)) {
+        elseif ($this->arg('f', $args)) $command .= ' -f';
+        if ($this->arg('update', $args) || $this->arg('u', $args)) {
             $command .= ' --update';
-        } elseif ($this->arg('all', $args)) {
+        } elseif ($this->arg('all', $args) || $this->arg('A', $args) || $this->arg('no-ignore-removal', $args)) {
             $command .= ' --all';
-        } elseif ($this->arg('no-all', $args)) {
+        } elseif ($this->arg('no-all', $args) || $this->arg('ignore-removal', $args)) {
             $command .= ' --no-all';
         }
         if ($this->arg('intent-to-add', $args)) $command .= ' --intent-to-add';
+        elseif ($this->arg('N', $args)) $command .= ' -N';
         if ($this->arg('refresh', $args)) $command .= ' --refresh';
         if ($this->arg('ignore-errors', $args)) $command .= ' --ignore-errors';
-        $command .= " {$paths}";
+        $command .= " -- {$paths}";
 
         $response = $this->git->exec($command);
         if ($response[0] !== 0 && !empty($response[2])) {
