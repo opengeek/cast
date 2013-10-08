d8975e88a3a9d929537fb4ba8dbfa73956a3a74e
Jason Coward <jason@opengeek.com>
2013-10-08 13:15:56 -0600

    Update phpdocs

--- src/Cast/Controllers/ControllerResponse.php
+++ src/Cast/Controllers/ControllerResponse.php
@@ -10,16 +10,30 @@
 
 namespace Cast\Controllers;
 
-
 use Cast\Response\CastResponse;
 
+/**
+ * A controller wrapper for a CastResponse.
+ *
+ * @package Cast\Controllers
+ */
 class ControllerResponse
 {
+    /** @var string The CastCommand string. */
     protected $command;
+    /** @var array An array of arguments from the CastCommand. */
     protected $arguments;
+    /** @var array An array of options from the CastCommand. */
     protected $options;
+    /** @var \Cast\Response\CastResponse The CastResponse object this object wraps. */
     protected $response;
 
+    /**
+     * Construct a new ControllerResponse instance
+     *
+     * @param ControllerInterface $controller The Controller building this response.
+     * @param CastResponse &$response A reference to the CastResponse to be wrapped by this instance.
+     */
     public function __construct(ControllerInterface $controller, CastResponse &$response)
     {
         $this->command = $controller->getCommand();
@@ -28,31 +42,61 @@ class ControllerResponse
         $this->response = &$response;
     }
 
+    /**
+     * Indicates if the CastCommand completed successfully.
+     *
+     * @return bool true if the response is a successful one.
+     */
     public function wasSuccessful()
     {
         return $this->response->wasSuccessful();
     }
 
+    /**
+     * The code returned by the execution of the actual Git command.
+     *
+     * @return integer The code returned by execution of a Git command.
+     */
     public function getCode()
     {
         return $this->response->getResult();
     }
 
+    /**
+     * Get the output from execution of the CastCommand.
+     *
+     * @return string The output from the CastCommand.
+     */
     public function getOutput()
     {
         return $this->response->getOutput();
     }
 
+    /**
+     * Error output from execution of the CastCommand.
+     *
+     * @return string Error output from the CastCommand.
+     */
     public function getErrors()
     {
         return $this->response->getErrors();
     }
 
+    /**
+     * Indicates if any errors were produced by the execution of the GitCommand.
+     *
+     * @return bool true if the error output from the CastCommand is not empty.
+     */
     public function hasErrors()
     {
         return $this->response->hasErrors();
     }
 
+    /**
+     * Return a human-readable string representation of the response.
+     *
+     * @return string A string representation of the complete response.
+     */
     public function __toString()
     {
         return $this->response->__toString();
--- src/Cast/Response/CastResponse.php
+++ src/Cast/Response/CastResponse.php
@@ -10,13 +10,26 @@
 
 namespace Cast\Response;
 
-
+/**
+ * Represents a response returned from a CastCommand being run().
+ *
+ * @package Cast\Response
+ */
 class CastResponse
 {
+    /** @var int The result code from the execution of a Git command. */
     protected $result;
+    /** @var string The output produced during execution of the CastCommand. */
     protected $output;
+    /** @var string Errors produced during execution of the CastCommand. */
     protected $errors;
 
+    /**
+     * Construct a new CastResponse instance.
+     *
+     * @param array $response An array returned from GitCommand::exec() containing an execution
+     * code, any output, and any errors produced by the process.
+     */
     public function __construct(array $response)
     {
         $this->result = (integer)$response[0];
@@ -24,31 +37,63 @@ class CastResponse
         $this->errors = $response[2];
     }
 
+    /**
+     * Get the result code produced by the CastCommand being executed.
+     *
+     * @return int The result code produced by execution of a Git command.
+     */
     public function getResult()
     {
         return $this->result;
     }
 
+    /**
+     * Get the output produced by the CastCommand being executed.
+     *
+     * @return string The output produced by the CastCommand being executed.
+     */
     public function getOutput()
     {
         return $this->output;
     }
 
+    /**
+     * Get the errors produced by the CastCommand being executed.
+     *
+     * @return string Any errors produced by the CastCommand being executed.
+     */
     public function getErrors()
     {
         return $this->errors;
     }
 
+    /**
+     * Indicates if the CastCommand execution produced any errors.
+     *
+     * @return bool true if the error output is empty, false if it is not.
+     */
     public function hasErrors()
     {
         return !empty($this->errors);
     }
 
+    /**
+     * Indicates if the CastCommand execution was successful.
+     *
+     * @return bool true if the execution was successful, false otherwise.
+     */
     public function wasSuccessful()
     {
         return $this->result === 0;
     }
 
+    /**
+     * Get a JSON representation of this response.
+     *
+     * @param int $options Options passed through to json_encode().
+     *
+     * @return string A JSON string representation of this response.
+     */
     public function toJSON($options = 0)
     {
         return json_encode(
@@ -61,6 +106,11 @@ class CastResponse
         );
     }
 
+    /**
+     * Return a human-readable string representation of this response.
+     *
+     * @return string A human-readable string representation of this response.
+     */
     public function __toString()
     {
         $output[] = "Command completed with code " . (string)$this->result;
